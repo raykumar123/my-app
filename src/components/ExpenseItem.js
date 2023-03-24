595a95d37d201fa36de34e1eda67a0cf7ad774cd
raykumar123 <eco20007@tezu.ac.in>
2023-03-24 13:51:41 +0530

    react-display-task

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+function ExpenseItem()
+{
+      return (
+      <div><div>
+        <h2>Expense Item</h2>
+        </div>
+        <div>
+            <h2>Food Rs 10</h2>
+            </div>
+            <div>
+                <h2>Petrol Rs 100</h2>
+            </div>
+            <div>
+                <h2>Movies Rs 200</h2>
+            </div>
+            </div>
+        );
+    
+}
+
+export default ExpenseItem;
