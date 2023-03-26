89b84b1ba0a9622d9fb3ac025fb166cefff9695e
raykumar123 <eco20007@tezu.ac.in>
2023-03-26 21:42:44 +0530

    date-styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item_description">
+    <div className='expense-item'>
+      <ExpenseDate date={props.date}/>
+      <div className='expense-item_description'>
         <h2>{props.title}</h2>
-        <div className="expense-item_price">${props.amount}</div>
-        <div>Location Of Expenditure</div>
+        <div className='expense-item_price'>${props.amount}</div>
+        {/* <div>Location Of Expenditure</div> */}
       </div>
     </div>
   );
