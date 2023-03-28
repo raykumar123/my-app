b967fd983f19b82955117309204a8b754ffe4da6
raykumar123 <eco20007@tezu.ac.in>
2023-03-28 15:46:33 +0530

    final-after-fat-arrow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Card from "./Expenses/UI/Card";
+import ExpenseItem from "./Expenses/ExpenseItem";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -22,50 +23,31 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     {/* <ExpenseItem
-  //       title={expenses[0].title}
-  //       amount={expenses[0].amount}
-  //       date={expenses[0].date}
-  //     ></ExpenseItem>
-  //     <ExpenseItem
-  //       title={expenses[1].title}
-  //       amount={expenses[1].amount}
-  //       date={expenses[1].date}
-  //     ></ExpenseItem>
-  //     <ExpenseItem
-  //       title={expenses[2].title}
-  //       amount={expenses[2].amount}
-  //       date={expenses[2].date}
-  //     ></ExpenseItem>
-  //     <ExpenseItem
-  //       title={expenses[3].title}
-  //       amount={expenses[3].amount}
-  //       date={expenses[3].date}
-  //     ></ExpenseItem> */}
-  //     {expenses.map((expense) => (
-  //       <ExpenseItem
-  //         key={expense.id}
-  //         title={expense.title}
-  //         amount={expense.amount}
-  //         date={expense.date}
-  //       />
-  //   </div>
-  // );
   return (
-    <div>
+    <Card>
       <h2>Let's get started!</h2>
-      {expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem> 
+    </Card>
+ 
   );
 }
 
--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -1,15 +1,16 @@
+import Card from "./UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item_description'>
         <h2>{props.title}</h2>
         <div className='expense-item_price'>${props.amount}</div>
         {/* <div>Location Of Expenditure</div> */}
       </div>
-    </div>
+    </Card>
   );
 }
 
