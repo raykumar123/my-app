46237452db151fc18fed1f4201f5fbb064bc7229
raykumar123 <eco20007@tezu.ac.in>
2023-03-29 14:08:26 +0530

    delete-expense

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -2,13 +2,16 @@ import Card from "./UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
+  const clickHandler = ()=> {
+    console.log('Clicked!!!!!')
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item_description'>
         <h2>{props.title}</h2>
         <div className='expense-item_price'>${props.amount}</div>
-        {/* <div>Location Of Expenditure</div> */}
+        <button onClick={clickHandler}>Delete Expense </button>
       </div>
     </Card>
   );
