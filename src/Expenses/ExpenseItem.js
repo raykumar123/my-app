9a3b375d7d1acf6374568962a1cc4dc09815a928
raykumar123 <eco20007@tezu.ac.in>
2023-03-31 00:29:24 +0530

    100$-change-expenses

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
+import React, { useState } from "react";
 import Card from "./UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+
 const ExpenseItem = (props) => {
-  const clickHandler = ()=> {
-    console.log('Clicked!!!!!')
-  }
+  const [amount, setAmount] = useState(props.amount);
+  console.log('ExpenseItem evaluted by React');
+ 
+  const clickHandler = () => {
+    setAmount('100$');
+    console.log(amount);
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item_description'>
         <h2>{props.title}</h2>
-        <div className='expense-item_price'>${props.amount}</div>
-        <button onClick={clickHandler}>Delete Expense </button>
+        <div className='expense-item_price'>${amount}</div>
+        <button onClick={clickHandler}>Change Amount </button>
       </div>
     </Card>
   );
