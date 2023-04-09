1d7ce4a6ed507505ef003af2b3a6840d6079d680
raykumar123 <eco20007@tezu.ac.in>
2023-04-09 12:56:30 +0530

    filter-feature

--- src/App.js
+++ src/App.js
@@ -4,67 +4,7 @@ import NewExpense from "./NewExpense/NewExpense";
 import Expenses from "./Expenses/Expenses";
 import React, { useState } from "react";
 
-// const App = () => {
-  
 
-//   const expenses = 
-//    [
-//     {
-//       id: "e1",
-//       title: "Toilet Paper",
-//       amount: 94.12,
-//       date: new Date(2020, 7, 14),
-//     },
-//     { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-//     {
-//       id: "e3",
-//       title: "Car Insurance",
-//       amount: 294.67,
-//       date: new Date(2021, 2, 28),
-//     },
-//     {
-//       id: "e4",
-//       title: "New Desk (Wooden)",
-//       amount: 450,
-//       date: new Date(2021, 5, 12),
-//     },
-//   ];
-
-
-//   const addExpenseHandler = expense =>{
-//     console.log('In App.js');
-//     console.log(expenses);
-//   }; 
-
-//   return (
-//     <div>
-//     <Card className='expenses'>
-//     <NewExpense onAddExpense={addExpenseHandler} />
-//       <ExpenseItem
-//         title={expenses[0].title}
-//         amount={expenses[0].amount}
-//         date={expenses[0].date}
-//       />
-//       <ExpenseItem
-//         title={expenses[1].title}
-//         amount={expenses[1].amount}
-//         date={expenses[1].date}
-//       />
-//       <ExpenseItem
-//         title={expenses[2].title}
-//         amount={expenses[2].amount}
-//         date={expenses[2].date}
-//       />
-//       <ExpenseItem
-//         title={expenses[3].title}
-//         amount={expenses[3].amount}
-//         date={expenses[3].date}
-//       />
-//     </Card>
-//     </div>
-//   );
-
-// };
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
@@ -94,7 +34,7 @@ const App = () => {
   const addExpenseHandler = expense => {
     setExpenses((prevExpenses) =>{
       return [expense, ...prevExpenses];
-    });
+    }); 
   }; 
 
   // return React.createElement(
