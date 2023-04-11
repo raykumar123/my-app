dd1130428771b86c59ac674caeb8650dfc9b6b00
raykumar123 <eco20007@tezu.ac.in>
2023-04-11 15:00:36 +0530

    final-one

--- src/Expenses/Expenses.js
+++ src/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import Card from "./UI/Card";
 import "./Expenses.css";
 import ExpenseList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -21,6 +22,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpenseList items={filteredExpenses} />
     </Card>
   );
